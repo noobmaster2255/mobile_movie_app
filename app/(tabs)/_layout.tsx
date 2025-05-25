47b0279c83b1f14a22bcc34c64b1b9fad7280a02
akhil <akhilsanil2000@gmail.com>
2025-05-25 17:03:49 -0400

    tabs completed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,59 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image, ImageBackground, Text, View } from "react-native";
 
+const TabIcons = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex felx-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
+      </ImageBackground>
+    );
+  }
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#a8b5db" className="size-5" />
+    </View>
+  );
+};
 const _Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0d23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0f0d23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcons focused={focused} icon={icons.home} title="Home" />
+          ),
         }}
       />
       <Tabs.Screen
@@ -16,6 +61,9 @@ const _Layout = () => {
         options={{
           title: "Saved",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcons focused={focused} icon={icons.save} title="Saved" />
+          ),
         }}
       />
       <Tabs.Screen
@@ -23,6 +71,9 @@ const _Layout = () => {
         options={{
           title: "Search",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcons focused={focused} icon={icons.search} title="Search" />
+          ),
         }}
       />
       <Tabs.Screen
@@ -30,6 +81,9 @@ const _Layout = () => {
         options={{
           title: "Profile",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcons focused={focused} icon={icons.person} title="Profile" />
+          ),
         }}
       />
     </Tabs>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,14 @@ module.exports = {
     extend: {
       colors: {
         primary: "#030014",
-        secondary: "151312",
+        secondary: "#151312",
         light: {
           100: "#d6c6ff",
           200: "#a8b5db",
           300: "#9ca4ab",
         },
         dark: {
-          100: "221f3d",
+          100: "#221f3d",
           200: "#ofod23",
         },
         accent: "#ab8bff",
